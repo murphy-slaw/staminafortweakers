430486db448393bdf4084e1c0eab50e9d35bfd5f
Kevin Murphy <kevin@eorbit.net>
2024-06-17 23:55:18 -0700

    Revert "Now all stamina changes happen in the ClientPlayerEntity only"
    This reverts commit e6b7e6a8

--- src/main/java/net/funkpla/staminafortweakers/mixin/LivingEntityMixin.java
+++ src/main/java/net/funkpla/staminafortweakers/mixin/LivingEntityMixin.java
@@ -5,13 +5,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -22,9 +17,6 @@ public abstract class LivingEntityMixin extends Entity {
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
-
-    @Shadow
-    public abstract boolean addStatusEffect(StatusEffectInstance instance);
 
     /**
      * Inject into LivingEntity.createLivingAttributes to add STAMINA and MAX_STAMINA attributes.
@@ -39,32 +31,5 @@ public abstract class LivingEntityMixin extends Entity {
     private static void addStaminaAttrs(final CallbackInfoReturnable<DefaultAttributeContainer.Builder> info) {
         info.getReturnValue().add(StaminaForTweakers.STAMINA);
         info.getReturnValue().add(StaminaForTweakers.MAX_STAMINA);
-    }
-
-    @Shadow
-    public abstract double getAttributeBaseValue(EntityAttribute attribute);
-
-    @Unique
-    public double getMaxStamina() {
-        return getAttributeBaseValue(StaminaForTweakers.MAX_STAMINA);
-    }
-
-    @Unique
-    public double getStamina() {
-        return getAttributeBaseValue(StaminaForTweakers.STAMINA);
-    }
-
-    @Shadow
-    public abstract EntityAttributeInstance getAttributeInstance(EntityAttribute attribute);
-
-    @Shadow
-    public abstract double getAttributeValue(EntityAttribute attribute);
-
-    @Shadow
-    public abstract boolean isClimbing();
-
-    @Unique
-    public void setStamina(double stamina) {
-        getAttributeInstance(StaminaForTweakers.STAMINA).setBaseValue(stamina);
     }
 }
