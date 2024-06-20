824760f5fb29c9a82e41121baf6430d767dcf5ab
Kevin Murphy <kevin@eorbit.net>
2024-06-20 15:27:18 -0700

    Add a new status effect, "Fatigue" instead of abusing slowness.

--- src/main/java/net/funkpla/staminafortweakers/StaminaForTweakers.java
+++ src/main/java/net/funkpla/staminafortweakers/StaminaForTweakers.java
@@ -1,10 +1,14 @@
 package net.funkpla.staminafortweakers;
 
+import de.dafuqs.additionalentityattributes.AdditionalEntityAttributes;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.attribute.ClampedEntityAttribute;
 import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
@@ -48,10 +52,15 @@ public class StaminaForTweakers implements ModInitializer {
     public static final Identifier BREATH_SCARED = new Identifier("staminafortweakers:breath_scared");
     public static SoundEvent ENTITY_PLAYER_PANT = SoundEvent.of(BREATH_SCARED);
 
+    public static final StatusEffect FATIGUE = new FatigueStatusEffect()
+            .addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "B7793F99-A88F-44E1-B19E-C753E9ACED3F", -0.15f, EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
+            .addAttributeModifier(CLIMB_SPEED, "2059DAB0-6417-46B1-AAA1-26BB473E773F", -0.15f, EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
+            .addAttributeModifier(AdditionalEntityAttributes.WATER_SPEED, "11D1DE67-7351-499C-BF48-4836D6EEC8FF", -0.15f, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
 
     @Override
     public void onInitialize() {
         AutoConfig.register(StaminaConfig.class, JanksonConfigSerializer::new);
+        Registry.register(Registries.STATUS_EFFECT, new Identifier("staminafortweakers", "fatigue"), FATIGUE);
         Registry.register(Registries.SOUND_EVENT, StaminaForTweakers.BREATH_SCARED, ENTITY_PLAYER_PANT);
     }
 }
