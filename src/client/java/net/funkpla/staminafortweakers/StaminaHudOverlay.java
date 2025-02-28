2d9ca177cc25e2395c0e98b18086c5a5f99f1489
Kevin Murphy <kevin@eorbit.net>
2025-02-27 21:11:52 -0800

    Smooth out stamina meter updates by using a short window moving average.

--- src/client/java/net/funkpla/staminafortweakers/StaminaHudOverlay.java
+++ src/client/java/net/funkpla/staminafortweakers/StaminaHudOverlay.java
@@ -1,6 +1,7 @@
 package net.funkpla.staminafortweakers;
 
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.math.Color;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.funkpla.staminafortweakers.config.StaminaConfig;
 import net.funkpla.staminafortweakers.registry.Attributes;
@@ -8,153 +9,158 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Vector2i;
 
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class StaminaHudOverlay implements HudRenderCallback {
-    private static final int opaque = 0xFF000000;
+    private static final int HistoryLength = 10;
+    private final static Queue<Float> history = new ArrayBlockingQueue<>(HistoryLength);
     private final StaminaConfig config = AutoConfig.getConfigHolder(StaminaConfig.class).getConfig();
 
-    private static RGBA splitColor(int color) {
-        float red = ((float) (color >> 16 & 0x0ff) / 255);
-        float green = ((float) (color >> 8 & 0x0ff) / 255);
-        float blue = ((float) (color & 0x0ff) / 255);
-        return new RGBA(red, green, blue, 1.0F);
+    private static float getDisplayStamina(LocalPlayer player) {
+        float scaledStamina =
+                (float) (player.getAttributeValue(Attributes.STAMINA) / player.getAttributeValue(Attributes.MAX_STAMINA));
+
+        if (!history.offer(scaledStamina)) {
+            history.poll();
+            history.add(scaledStamina);
+        }
+
+        return history.size() == HistoryLength ?
+                history.stream().reduce(Float::sum).orElse(scaledStamina) / HistoryLength : scaledStamina;
     }
 
     @Override
     public void onHudRender(GuiGraphics context, float tickDelta) {
-        if ((int) tickDelta % 10 != 0) return;
         int width = context.guiWidth();
         int height = context.guiHeight();
         Minecraft client = Minecraft.getInstance();
         double scale = client.getWindow().getGuiScale();
 
-        LocalPlayer player = Minecraft.getInstance().player;
-        if (client.options.hideGui || player.isCreative() || player.isSpectator()) return;
-        float scaledStamina = (float) (player.getAttributeValue(Attributes.STAMINA) / player.getAttributeValue(Attributes.MAX_STAMINA));
-        float pct = scaledStamina * 100;
+        LocalPlayer player = Objects.requireNonNull(Minecraft.getInstance().player);
+        if (client.options.hideGui || player.isCreative() || player.isSpectator())
+            return;
 
-        int color = getColor(pct);
-
-        int x1 = 0;
-        int y1 = 0;
+        float displayStamina = getDisplayStamina(player);
+        Color color = getColor(displayStamina * 100);
 
         if (config.hudMode == StaminaConfig.HudMode.BAR) {
-            int barWidth = 0;
-            int barHeight = 0;
-
-            switch (config.bar.orientation) {
-                case VERTICAL -> {
-                    barWidth = config.bar.shortSide;
-                    barHeight = config.bar.longSide;
-                }
-                case HORIZONTAL -> {
-                    barWidth = config.bar.longSide;
-                    barHeight = config.bar.shortSide;
-                }
-
-            }
-
-            switch (config.alignV) {
-                case TOP -> y1 = config.bar.offsetY;
-                case CENTER -> y1 = height / 2 - config.bar.offsetY - barHeight / 2;
-                case BOTTOM -> y1 = height - config.bar.offsetY - barHeight;
-            }
-
-            switch (config.alignH) {
-                case LEFT -> x1 = config.bar.offsetX;
-                case CENTER -> x1 = width / 2 - config.bar.offsetX - barWidth / 2;
-                case RIGHT -> x1 = width - config.bar.offsetX - barWidth;
-            }
-            int x2 = x1 + barWidth;
-            int y2 = y1 + barHeight;
-
-            context.fill(x1 - 1, y1 - 1, x2 + 1, y2 + 1, -1, opaque + config.staminaBarOutlineColor);
-            context.fill(x1, y1, x2, y2, -1, opaque + config.staminaBarBackgroundColor);
-
-            if (config.bar.orientation == StaminaConfig.Orientation.HORIZONTAL)
-                context.fill(x1, y1, (int) (x1 + (barWidth * scaledStamina)), y2, -1, color);
-            else context.fill(x1, y2, x2, y2 - (int) (barHeight * scaledStamina), -1, color);
-
-            if (!((Exhaustible) player).shouldExhaust()) {
-                context.renderOutline(x1 - 2, y1 - 2, barWidth + 4, barHeight + 4, opaque + config.staminaBarTirelessColor);
-            }
-
+            renderStaminaBar(context, height, width, displayStamina, color, (Exhaustible) player);
         } else {
-
-            ResourceLocation icon;
-            ResourceLocation bgIcon;
-            if (player.isSprinting()) {
-                icon = StaminaMod.locate("textures/stamina/sprint.png");
-                bgIcon = StaminaMod.locate("textures/stamina/sprint_background.png");
-            } else {
-                icon = StaminaMod.locate("textures/stamina/walk.png");
-                bgIcon = StaminaMod.locate("textures/stamina/walk_background.png");
-            }
-            Vector2i iconDimensions = StaminaClientMod.getIconSize(icon);
-
-            int iconHeight = config.icon.height;
-            int imageWidth = iconDimensions.x;
-            int imageHeight = iconDimensions.y;
-            float iconProportions = ((float) imageWidth / (float) imageHeight);
-            int iconWidth = (int) (iconHeight * iconProportions);
-
-            switch (config.alignV) {
-                case TOP -> y1 = (int) (config.icon.offsetY / scale);
-                case CENTER -> y1 = height / 2 - config.icon.offsetY - iconHeight / 2;
-                case BOTTOM -> y1 = height - config.icon.offsetY - iconHeight;
-            }
-
-            switch (config.alignH) {
-                case LEFT -> x1 = (int) (config.icon.offsetX / scale);
-                case CENTER -> x1 = width / 2 - config.icon.offsetX - iconWidth / 2;
-                case RIGHT -> x1 = width - config.icon.offsetX - iconWidth;
-            }
-
-            RGBA bg = splitColor(config.staminaBarBackgroundColor);
-
-            context.setColor(bg.red, bg.green, bg.blue, 1);
-            context.blit(
-                    bgIcon,
-                    x1, y1,
-                    0, 0,
-                    iconWidth, iconHeight,
-                    iconWidth, iconHeight
-            );
-
-            int cutout = (int) (iconHeight * scaledStamina);
-            int remainder = iconHeight - cutout;
-            if (!((Exhaustible) player).shouldExhaust()) {
-                color = config.staminaBarTirelessColor;
-            }
-
-            RGBA split = splitColor(color);
-            context.setColor(split.red, split.green, split.blue, 1);
-            context.blit(
-                    icon,
-                    x1, y1 + remainder,
-                    0, remainder,
-                    iconWidth, cutout,
-                    iconWidth, iconHeight
-            );
-            context.setColor(1, 1, 1, 1);
+            renderStaminaIcon(context, player, scale, height, width, displayStamina, color);
         }
     }
 
-    private int getColor(float pct) {
-        int color = opaque;
+    private void renderStaminaIcon(GuiGraphics context, @NotNull LocalPlayer player, double scale, int height,
+                                   int width, float displayStamina, Color color) {
+        int y1;
+        int x1;
+        ResourceLocation icon;
+        ResourceLocation bgIcon;
+        if (player.isSprinting()) {
+            icon = StaminaMod.locate("textures/stamina/sprint.png");
+            bgIcon = StaminaMod.locate("textures/stamina/sprint_background.png");
+        } else {
+            icon = StaminaMod.locate("textures/stamina/walk.png");
+            bgIcon = StaminaMod.locate("textures/stamina/walk_background.png");
+        }
+        Vector2i iconDimensions = StaminaClientMod.getIconSize(icon);
+
+        int iconHeight = config.icon.height;
+        int imageWidth = iconDimensions.x;
+        int imageHeight = iconDimensions.y;
+        float iconProportions = ((float) imageWidth / (float) imageHeight);
+        int iconWidth = (int) (iconHeight * iconProportions);
+
+        switch (config.alignV) {
+            case TOP -> y1 = (int) (config.icon.offsetY / scale);
+            case CENTER -> y1 = height / 2 - config.icon.offsetY - iconHeight / 2;
+            default -> y1 = height - config.icon.offsetY - iconHeight;
+        }
+
+        switch (config.alignH) {
+            case LEFT -> x1 = (int) (config.icon.offsetX / scale);
+            case CENTER -> x1 = width / 2 - config.icon.offsetX - iconWidth / 2;
+            default -> x1 = width - config.icon.offsetX - iconWidth;
+        }
+
+        Color bg = Color.ofOpaque(config.staminaBarBackgroundColor);
+
+        context.setColor(bg.getRed(), bg.getGreen(), bg.getBlue(), bg.getAlpha());
+        context.blit(bgIcon, x1, y1, 0, 0, iconWidth, iconHeight, iconWidth, iconHeight);
+
+        int cutout = (int) (iconHeight * displayStamina);
+        int remainder = iconHeight - cutout;
+
+        Color fillColor;
+        if (((Exhaustible) player).shouldExhaust()) {
+            fillColor = color;
+        } else {
+            fillColor = Color.ofOpaque(config.staminaBarTirelessColor);
+        }
+
+        context.setColor(fillColor.getRed(), fillColor.getGreen(), fillColor.getBlue(), fillColor.getAlpha());
+        context.blit(icon, x1, y1 + remainder, 0, remainder, iconWidth, cutout, iconWidth, iconHeight);
+        context.setColor(1, 1, 1, 1);
+    }
+
+    private void renderStaminaBar(GuiGraphics context, int height, int width, float displayStamina, Color color,
+                                  Exhaustible player) {
+        int x1;
+        int y1;
+        int barWidth;
+        int barHeight;
+
+        if (Objects.requireNonNull(config.bar.orientation) == StaminaConfig.Orientation.VERTICAL) {
+            barWidth = config.bar.shortSide;
+            barHeight = config.bar.longSide;
+        } else {
+            barWidth = config.bar.longSide;
+            barHeight = config.bar.shortSide;
+        }
+
+        switch (config.alignV) {
+            case TOP -> y1 = config.bar.offsetY;
+            case CENTER -> y1 = height / 2 - config.bar.offsetY - barHeight / 2;
+            default -> y1 = height - config.bar.offsetY - barHeight;
+        }
+
+        switch (config.alignH) {
+            case LEFT -> x1 = config.bar.offsetX;
+            case CENTER -> x1 = width / 2 - config.bar.offsetX - barWidth / 2;
+            default -> x1 = width - config.bar.offsetX - barWidth;
+        }
+        int x2 = x1 + barWidth;
+        int y2 = y1 + barHeight;
+
+        context.fill(x1 - 1, y1 - 1, x2 + 1, y2 + 1, -1, Color.ofOpaque(config.staminaBarOutlineColor).getColor());
+        context.fill(x1, y1, x2, y2, -1, Color.ofOpaque(config.staminaBarBackgroundColor).getColor());
+
+        if (config.bar.orientation == StaminaConfig.Orientation.HORIZONTAL)
+            context.fill(x1, y1, x1 + (int) (barWidth * displayStamina), y2, -1, color.getColor());
+        else
+            context.fill(x1, y2, x2, y2 - (int) (barHeight * displayStamina), -1, color.getColor());
+
+        if (!player.shouldExhaust()) {
+            context.renderOutline(x1 - 1, y1 - 1, barWidth + 2, barHeight + 2,
+                    Color.ofOpaque(config.staminaBarTirelessColor).getColor());
+        }
+    }
+
+    private @NotNull Color getColor(float pct) {
         if (pct <= Math.max(config.windedPercentage / 2.0F, 10F)) {
-            color += config.staminaBarNearlyExhaustedColor;
-        } else if (pct <= config.windedPercentage) {
-            color += config.staminaBarWindedColor;
-        } else if (pct <= config.fatiguedPercentage) {
-            color += config.staminaBarFatiguedColor;
-        } else {
-            color += config.staminaBarColor;
+            return Color.ofOpaque(config.staminaBarNearlyExhaustedColor);
         }
-        return color;
-    }
-
-    private record RGBA(float red, float green, float blue, float alpha) {
+        if (pct <= config.windedPercentage) {
+            return Color.ofOpaque(config.staminaBarWindedColor);
+        }
+        if (pct <= config.fatiguedPercentage) {
+            return Color.ofOpaque(config.staminaBarFatiguedColor);
+        }
+        return Color.ofOpaque(config.staminaBarColor);
     }
 }
