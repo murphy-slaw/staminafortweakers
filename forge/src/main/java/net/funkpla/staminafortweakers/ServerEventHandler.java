c2dd837d623e6ddd1aa1e9afa209cf78d4a85424
Kevin Murphy <kevin@eorbit.net>
2025-07-26 23:59:17 -0700

    Add missing event handlers to Forge version.

---
+++ forge/src/main/java/net/funkpla/staminafortweakers/ServerEventHandler.java
@@ -0,0 +1,38 @@
+package net.funkpla.staminafortweakers;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class ServerEventHandler {
+
+  @SubscribeEvent
+  public static void handleAttack(AttackEntityEvent event) {
+    Player player = event.getEntity();
+    if (player.level().isClientSide || player.isCreative() || player.isSpectator()) return;
+    ((Attacker) player).setAttacked(true);
+  }
+
+  @SubscribeEvent
+  public static void handleBlockBreak(BlockEvent.BreakEvent event) {
+    Player player = event.getPlayer();
+    if (player.level().isClientSide || player.isCreative() || player.isSpectator()) return;
+    ((Miner) player).depleteStaminaForBlockBreak();
+  }
+
+  @SubscribeEvent
+  public static void handleUseShield(PlayerInteractEvent.RightClickItem event) {
+    Player player = event.getEntity();
+    if (player.isCreative() || player.isSpectator()) return;
+    if (player.getItemInHand(event.getHand()).getItem() == Items.SHIELD) {
+      if (((Exhaustible) player).isExhausted()) {
+        event.setCanceled(true);
+      }
+    }
+  }
+}
