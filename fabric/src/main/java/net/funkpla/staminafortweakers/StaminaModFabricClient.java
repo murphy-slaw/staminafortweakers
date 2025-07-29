8ed6b3ef717ec794d6efc31b792ff500fec02283
Kevin Murphy <kevin@eorbit.net>
2025-07-29 00:02:46 -0700

    Fix strange color problems with icon mode caused by passing ints instead of floats.

--- fabric/src/main/java/net/funkpla/staminafortweakers/StaminaModFabricClient.java
+++ fabric/src/main/java/net/funkpla/staminafortweakers/StaminaModFabricClient.java
@@ -1,66 +1,26 @@
 package net.funkpla.staminafortweakers;
 
-import com.mojang.blaze3d.platform.NativeImage;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.resources.Resource;
-import net.minecraft.server.packs.resources.ResourceManager;
-import org.jetbrains.annotations.NotNull;
-import org.joml.Vector2i;
 
 public class StaminaModFabricClient implements ClientModInitializer {
-  private static final HashMap<ResourceLocation, Vector2i> ICON_SIZES = new HashMap<>();
-
-  public static Vector2i getIconSize(ResourceLocation rl) {
-    return ICON_SIZES.get(rl);
-  }
 
   @Override
   public void onInitializeClient() {
-    HudRenderCallback.EVENT.register(new StaminaHudOverlay());
+    ClientLifecycleEvents.CLIENT_STARTED.register(
+        client -> HudRenderCallback.EVENT.register(new StaminaHudOverlay()));
 
-    ArrayList<ResourceLocation> icons = new ArrayList<>();
-    icons.add(Common.locate("textures/stamina/walk.png"));
-    icons.add(Common.locate("textures/stamina/sprint.png"));
-
+    IconManagerFabric.init();
     ResourceManagerHelper.get(PackType.CLIENT_RESOURCES)
-        .registerReloadListener(
-            new SimpleSynchronousResourceReloadListener() {
-              @Override
-              public ResourceLocation getFabricId() {
-                return ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "resources");
-              }
-
-              @Override
-              public void onResourceManagerReload(@NotNull ResourceManager resourceManager) {
-                for (ResourceLocation icon : icons) {
-                  try {
-                    Resource resource = resourceManager.getResourceOrThrow(icon);
-                    try (InputStream inputStream = resource.open()) {
-                      NativeImage nativeImage = NativeImage.read(inputStream);
-                      ICON_SIZES.put(
-                          icon, new Vector2i(nativeImage.getWidth(), nativeImage.getHeight()));
-                    } catch (IOException e) {
-                      Constants.LOG.warn("Unable to read icon texture {}", icon);
-                    }
-                  } catch (FileNotFoundException e) {
-                    Constants.LOG.warn("Icon texture '{}' not found", icon);
-                  }
-                }
-              }
-            });
+        .registerReloadListener(new IconManagerFabric());
 
     FabricLoader.getInstance()
         .getModContainer(Constants.MOD_ID)
@@ -71,5 +31,13 @@ public class StaminaModFabricClient implements ClientModInitializer {
                     container,
                     Component.literal("Old Style Stamina Icons"),
                     ResourcePackActivationType.NORMAL));
+  }
+
+  private static class IconManagerFabric extends IconManager
+      implements IdentifiableResourceReloadListener {
+    @Override
+    public ResourceLocation getFabricId() {
+      return Common.locate("resources");
+    }
   }
 }
