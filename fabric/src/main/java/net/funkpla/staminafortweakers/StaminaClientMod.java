4491399338d4032d109cab30dd85f4abdefad605
Kevin Murphy <kevin@eorbit.net>
2025-07-29 23:16:18 -0700

    New pixel art icons (with old icons as an optional resource pack).

--- fabric/src/main/java/net/funkpla/staminafortweakers/StaminaClientMod.java
+++ fabric/src/main/java/net/funkpla/staminafortweakers/StaminaClientMod.java
@@ -5,6 +5,9 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
 
@@ -12,20 +15,29 @@ public class StaminaClientMod implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
-    ClientLifecycleEvents.CLIENT_STARTED.register(client->{
-      HudRenderCallback.EVENT.register(new StaminaHudOverlay());
-    });
+    ClientLifecycleEvents.CLIENT_STARTED.register(
+        client -> HudRenderCallback.EVENT.register(new StaminaHudOverlay()));
+
     IconManagerFabric.init();
     ResourceManagerHelper.get(PackType.CLIENT_RESOURCES)
         .registerReloadListener(new IconManagerFabric());
+
+    FabricLoader.getInstance()
+        .getModContainer(Constants.MOD_ID)
+        .ifPresent(
+            container ->
+                ResourceManagerHelper.registerBuiltinResourcePack(
+                    Common.locate("old_style_icons"),
+                    container,
+                    Component.literal("Old Style Stamina Icons"),
+                    ResourcePackActivationType.NORMAL));
   }
 
   private static class IconManagerFabric extends IconManager
       implements IdentifiableResourceReloadListener {
     @Override
     public ResourceLocation getFabricId() {
-      return new ResourceLocation(Constants.MOD_ID, "resources");
+      return Common.locate("resources");
     }
   }
-
 }
--- forge/src/main/java/net/funkpla/staminafortweakers/StaminaModForge.java
+++ forge/src/main/java/net/funkpla/staminafortweakers/StaminaModForge.java
@@ -1,9 +1,18 @@
 package net.funkpla.staminafortweakers;
 
+import java.nio.file.Path;
 import net.funkpla.staminafortweakers.registry.*;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.PathPackResources;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -20,6 +29,27 @@ public class StaminaModForge {
     Enchantments.registerEnchantments();
     DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ClientSetup::new);
     modBus.addListener(Attributes::addLivingEntityAttributes);
+    modBus.addListener(StaminaModForge::addPackFinders);
   }
 
+  @SubscribeEvent
+  public static void addPackFinders(AddPackFindersEvent event) {
+    if (event.getPackType() == PackType.CLIENT_RESOURCES) {
+      Path resourcePath =
+          ModList.get()
+              .getModFileById(Constants.MOD_ID)
+              .getFile()
+              .findResource("resourcepacks/old_style_icons");
+      Pack pack =
+          Pack.readMetaAndCreate(
+              "Old Style Icons",
+              Component.literal("Old Style Icons"),
+              false,
+              (path) -> new PathPackResources(path, resourcePath, false),
+              PackType.CLIENT_RESOURCES,
+              Pack.Position.BOTTOM,
+              PackSource.BUILT_IN);
+      event.addRepositorySource(packConsumer -> packConsumer.accept(pack));
+    }
+  }
 }
