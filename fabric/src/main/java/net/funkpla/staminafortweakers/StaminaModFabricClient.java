80b153dc404c5be594cc53c858d7891b2e07d4f9
Kevin Murphy <kevin@eorbit.net>
2025-07-29 23:16:18 -0700

    New pixel art icons (with old icons as an optional resource pack).

--- fabric/src/main/java/net/funkpla/staminafortweakers/StaminaModFabricClient.java
+++ fabric/src/main/java/net/funkpla/staminafortweakers/StaminaModFabricClient.java
@@ -1,10 +1,18 @@
 package net.funkpla.staminafortweakers;
 
 import com.mojang.blaze3d.platform.NativeImage;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.Resource;
@@ -12,51 +20,56 @@ import net.minecraft.server.packs.resources.ResourceManager;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector2i;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class StaminaModFabricClient implements ClientModInitializer {
-    private static final HashMap<ResourceLocation, Vector2i> ICON_SIZES = new HashMap<>();
+  private static final HashMap<ResourceLocation, Vector2i> ICON_SIZES = new HashMap<>();
 
-    @Override
-    public void onInitializeClient() {
-        HudRenderCallback.EVENT.register(new StaminaHudOverlay());
+  public static Vector2i getIconSize(ResourceLocation rl) {
+    return ICON_SIZES.get(rl);
+  }
 
-        ArrayList<ResourceLocation> icons = new ArrayList<>();
-        icons.add(Common.locate("textures/stamina/walk.png"));
-        icons.add(Common.locate("textures/stamina/sprint.png"));
+  @Override
+  public void onInitializeClient() {
+    HudRenderCallback.EVENT.register(new StaminaHudOverlay());
 
-        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
-            @Override
-            public ResourceLocation getFabricId() {
+    ArrayList<ResourceLocation> icons = new ArrayList<>();
+    icons.add(Common.locate("textures/stamina/walk.png"));
+    icons.add(Common.locate("textures/stamina/sprint.png"));
+
+    ResourceManagerHelper.get(PackType.CLIENT_RESOURCES)
+        .registerReloadListener(
+            new SimpleSynchronousResourceReloadListener() {
+              @Override
+              public ResourceLocation getFabricId() {
                 return ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "resources");
-            }
+              }
 
-            @Override
-            public void onResourceManagerReload(@NotNull ResourceManager resourceManager) {
+              @Override
+              public void onResourceManagerReload(@NotNull ResourceManager resourceManager) {
                 for (ResourceLocation icon : icons) {
-                    try {
-                        Resource resource = resourceManager.getResourceOrThrow(icon);
-                        try (InputStream inputStream = resource.open()) {
-                            NativeImage nativeImage = NativeImage.read(inputStream);
-                            ICON_SIZES.put(icon, new Vector2i(nativeImage.getWidth(), nativeImage.getHeight()));
-                        } catch (IOException e) {
-                            Constants.LOG.warn("Unable to read icon texture {}", icon);
-                        }
-                    } catch (FileNotFoundException e) {
-                        Constants.LOG.warn("Icon texture '{}' not found", icon);
+                  try {
+                    Resource resource = resourceManager.getResourceOrThrow(icon);
+                    try (InputStream inputStream = resource.open()) {
+                      NativeImage nativeImage = NativeImage.read(inputStream);
+                      ICON_SIZES.put(
+                          icon, new Vector2i(nativeImage.getWidth(), nativeImage.getHeight()));
+                    } catch (IOException e) {
+                      Constants.LOG.warn("Unable to read icon texture {}", icon);
                     }
+                  } catch (FileNotFoundException e) {
+                    Constants.LOG.warn("Icon texture '{}' not found", icon);
+                  }
                 }
-            }
-        });
-    }
+              }
+            });
 
-    public static Vector2i getIconSize(ResourceLocation rl) {
-        return ICON_SIZES.get(rl);
-    }
-
-
+    FabricLoader.getInstance()
+        .getModContainer(Constants.MOD_ID)
+        .ifPresent(
+            container ->
+                ResourceManagerHelper.registerBuiltinResourcePack(
+                    Common.locate("old_style_icons"),
+                    container,
+                    Component.literal("Old Style Stamina Icons"),
+                    ResourcePackActivationType.NORMAL));
+  }
 }
--- neoforge/src/main/java/net/funkpla/staminafortweakers/StaminaModNeoForgeClient.java
+++ neoforge/src/main/java/net/funkpla/staminafortweakers/StaminaModNeoForgeClient.java
@@ -4,13 +4,19 @@ import me.shedaniel.autoconfig.AutoConfig;
 import net.funkpla.staminafortweakers.config.StaminaConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
 
 @Mod(value = Constants.MOD_ID, dist = Dist.CLIENT)
 public class StaminaModNeoForgeClient {
@@ -22,7 +28,19 @@ public class StaminaModNeoForgeClient {
     if (resManager instanceof ReloadableResourceManager) {
       ((ReloadableResourceManager) resManager).registerReloadListener(new IconManager());
       modBus.register(HudOverlayHandler.class);
+      modBus.register(this);
     }
+  }
+
+  @SubscribeEvent
+  public void addPackFinders(AddPackFindersEvent event) {
+    event.addPackFinders(
+        Common.locate("resourcepacks/old_style_icons"),
+        PackType.CLIENT_RESOURCES,
+        Component.literal("Old Style Stamina Icons"),
+        PackSource.BUILT_IN,
+        false,
+        Pack.Position.BOTTOM);
   }
 
   private Screen createScreen(ModContainer modContainer, Screen parent) {
