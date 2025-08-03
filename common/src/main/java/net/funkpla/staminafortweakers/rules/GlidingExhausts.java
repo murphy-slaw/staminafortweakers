f5d3db8a16140716dde400d3fc1c9e78abe61c7a
Kevin Murphy <kevin@eorbit.net>
2025-08-03 10:35:24 -0700

    Don't activate GlidingExhausts rule if Gliders mod isn't loaded.
    References: #45

--- common/src/main/java/net/funkpla/staminafortweakers/rules/GlidingExhausts.java
+++ common/src/main/java/net/funkpla/staminafortweakers/rules/GlidingExhausts.java
@@ -3,21 +3,22 @@ package net.funkpla.staminafortweakers.rules;
 import net.funkpla.staminafortweakers.Exhaustible;
 import net.funkpla.staminafortweakers.compat.vc_gliders.VCGlidersCompat;
 import net.funkpla.staminafortweakers.config.StaminaConfig;
+import net.funkpla.staminafortweakers.platform.Services;
 import net.minecraft.server.level.ServerPlayer;
 
-public class GlidingExhausts implements IRule{
-    @Override
-    public boolean shouldEnable(StaminaConfig config) {
-        return config.depletionPerGliderTick > 0;
-    }
+public class GlidingExhausts implements IRule {
+  @Override
+  public boolean shouldEnable(StaminaConfig config) {
+    return Services.PLATFORM.isModLoaded("vc_gliders") && config.depletionPerGliderTick > 0;
+  }
 
-    @Override
-    public boolean test(ServerPlayer player) {
-        return VCGlidersCompat.isGliding(player);
-    }
+  @Override
+  public boolean test(ServerPlayer player) {
+    return Services.PLATFORM.isModLoaded("vc_gliders") && VCGlidersCompat.isGliding(player);
+  }
 
-    @Override
-    public void doResult(StaminaConfig config, ServerPlayer player) {
+  @Override
+  public void doResult(StaminaConfig config, ServerPlayer player) {
     ((Exhaustible) player).depleteStamina((config.depletionPerGliderTick));
-    }
+  }
 }
