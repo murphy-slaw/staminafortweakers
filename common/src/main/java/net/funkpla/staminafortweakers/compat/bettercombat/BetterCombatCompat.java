84041674a91c2d412eab834bdebd7ca92f90ee55
Kevin Murphy <kevin@eorbit.net>
2025-08-04 18:38:56 -0700

    Don't check weapon tags on Better Combat AttackStart events.

--- common/src/main/java/net/funkpla/staminafortweakers/compat/bettercombat/BetterCombatCompat.java
+++ common/src/main/java/net/funkpla/staminafortweakers/compat/bettercombat/BetterCombatCompat.java
@@ -1,14 +1,16 @@
 package net.funkpla.staminafortweakers.compat.bettercombat;
 
 import net.bettercombat.api.client.BetterCombatClientEvents;
-import net.funkpla.staminafortweakers.Common;
 import net.funkpla.staminafortweakers.platform.Services;
 
 public class BetterCombatCompat {
   public static void init() {
     if (Services.PLATFORM.isModLoaded("bettercombat")) {
       BetterCombatClientEvents.ATTACK_START.register(
-          (player, hand) -> Common.trySwingPacket(player));
+          (player, hand) -> {
+            if (!(player.isCreative() || player.isSpectator()))
+              Services.PACKET.sendWeaponSwingPacket();
+          });
     }
   }
 }
